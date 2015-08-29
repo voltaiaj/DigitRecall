42c3fbbcee3a6811f2f5f0905ac2561301e17082
voltaiaj <alexander.voltaire@gmail.com>
2015-08-28 23:56:19 -0500

    Initial project commit

---
+++ CSharpConsoleApp/CSharpConsoleApp/BasicClassifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleApp
+{
+    public class BasicClassifier : IClassifier
+    {
+        private IEnumerable<Observation> _data;
+        private readonly IDistance _distance;
+        
+        public BasicClassifier(IDistance distance)
+        {
+            this._distance = distance;
+        }
+
+        public void Train(IEnumerable<Observation> trainingSet)
+        {
+            this._data = trainingSet;
+        }
+
+        public string Predict(int[] pixels)
+        {
+            Observation currentBest = null;
+            var shortest = Double.MaxValue;
+
+            foreach (Observation obs in this._data)
+            {
+                var dist = this._distance.Between(obs.Pixels, pixels);
+                if (dist < shortest)
+                {
+                    shortest = dist;
+                    currentBest = obs;
+                }
+            }
+
+            return currentBest.Label;
+        }
+    }
+}
---
+++ CSharpConsoleApp/CSharpConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var distance = new ManhattanDistance();
+            var classifier = new BasicClassifier(distance);
+
+            var trainingPath = @"C:\Users\Alexander\Downloads\trainingsample.csv";
+            var training = DataReader.ReadObservations(trainingPath);
+            classifier.Train(training);
+
+            var validationaPath = @"C:\Users\Alexander\Downloads\validationsample.csv";
+            var validation = DataReader.ReadObservations(validationaPath);
+
+            var correct = Evaluator.Correct(validation, classifier);
+            Console.WriteLine("Correctly classified: {0:P2}", correct);
+
+            Console.ReadLine();
+        }
+    }
+}
