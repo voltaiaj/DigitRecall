821dec735182956d88e560dcaa786ef86ecf3ff0
voltaiaj <alexander.voltaire@gmail.com>
2015-08-30 23:54:12 -0500

    Adding Script.fsx

--- CSharpConsoleApp/Library1/Script.fsx
+++ CSharpConsoleApp/Library1/Script.fsx
@@ -1,8 +1,16 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿open System.IO
+type Observation = { Label: string; Pixels: int[] }
 
-#load "Library1.fs"
-open Library1
+let toObservation (csvData:string) = 
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
 
-// Define your library scripting code here
+let reader path = 
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
 
+let trainingPath = @"C:\Users\Alexander\Downloads\trainingsample.csv"
+let trainingData = reader trainingPath
